10459c9cbc306e16d55331448adb9afdf5d7ae21
faris117 <sanmanulfaris@gmail.com>
2025-05-04 12:51:41 +0530

    removed unwanted text

--- src/components/CreateQuizPage.tsx
+++ src/components/CreateQuizPage.tsx
@@ -9,11 +9,10 @@ interface Props {
 export default function CreateQuizPage({ user }: Props) {
   return (
     <div className="outer-container">
-    <div className="home-container">
-      <Navbar username={user.username}/>
-      <h2>Create Quiz Page</h2>
-      <p>Welcome, {user.username}! Here you can create your quiz.</p>
-      <QuizDetailForm/>
+      <div className="home-container">
+        <Navbar username={user.username}/>
+        
+        <QuizDetailForm/>
     </div>
     </div>
   );
